35404aa3dddad7da0534d21ec92676cd6ecd7348
Leleouch14 <paramshivanshthor@gmail.com>
2026-09-06 00:38:52 +0530

    completing relational op

--- Relation_OP.cpp
+++ Relation_OP.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+ int main() {
     int num1{}, num2{};
     cout << boolalpha;
-    cout << "enter 2 int seperated by space: ";
-    cin >> num1 >> num2;
-    cout << "Num1 is: " << num1 << " Num2 is: " << num2 << endl;
-    cout << "Num1 > Num2: " << (num1>num2) << endl;
-    cout << "num1 < num2: " << (num1<num2) << endl;
-    cout << "num1 >= num2: " << (num1>=num2) << endl;
-    cout << "num1 <= num2: " << (num1<=num2) << endl;
-}
+//     cout << "enter 2 int seperated by space: ";
+//     cin >> num1 >> num2;
+//     cout << "Num1 is: " << num1 << " Num2 is: " << num2 << endl;
+//     cout << "Num1 > Num2: " << (num1>num2) << endl;
+//     cout << "num1 < num2: " << (num1<num2) << endl;
+//     cout << "num1 >= num2: " << (num1>=num2) << endl;
+//     cout << "num1 <= num2: " << (num1<=num2) << endl;
+
+const int lower(10);
+const int upper(20);
+cout << "\nenter an integer that is greater than: " << lower << " and less than "<< upper << endl;
+cin >> num1;
+
+bool within_bounds(false);
+// within_bounds=(num1>lower && num1<upper);
+// cout << num1 << " is between bounds? " << within_bounds << endl;
+bool outside_bounds(false);
+outside_bounds=(num1<lower || num1>upper);
+cout << "the number: " << num1 << " is outside bounds? " << outside_bounds << endl;
+
+ }
--- second.cpp
+++ second.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 int main() {
     int arr[] {1, 5, 6, 8};
-    cout << arr[2] << "\n Is the value" << endl;
-   int var(0);
-cin >> var;
-arr[2] = var;
-cout << "\nupdate value: " << arr;
- return 0;
+    cout << arr[2];
+    return 0;
 }
